6a68d8e11193ebc587fc47018fcf0162d3b55c79
9699981954 <s9699981954@gmail.com>
2023-12-12 08:01:56 +0300

    2/3/1

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,23 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+
+    List<User> getAllUsers();
+
+    User getUserById(int id);
+
+
+    void saveUser(User user);
+
+    void updateUser(User updatedUser);
+
+
+    void deleteUser(int id);
+
+
+}
---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDao;
+import web.dao.UserDaoImpl;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return userDao.getUserById(id);
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Override
+    public void updateUser(User updatedUser) {
+        userDao.updateUser(updatedUser);
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        userDao.deleteUser(id);
+    }
+}
